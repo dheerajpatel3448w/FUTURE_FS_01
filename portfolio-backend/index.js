1f351355c2d61ef7dc2cf625b6f94292ce30edbf
dheerajpatel3448w <dheerajdominetar3448@gmail.com>
2025-08-09 16:43:33 +0530

    Initial commit

--- portfolio-backend/index.js
+++ portfolio-backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-     origin: "https://your-frontend.vercel.app", // Vercel deployed link
+     origin: "https://devdheerajportfolio.vercel.app", // Vercel deployed link
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
